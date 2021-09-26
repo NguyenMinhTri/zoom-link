f0c5c27d7549e733c70605883aaf021967eaf39b
NguyenMinhTri <nmtri.uit@gmail.com>
2021-09-26 15:26:01 +0700

    update

--- force-open.js
+++ force-open.js
@@ -1,14 +1,20 @@
-window.location = 'intent:http://zoom.iotdaiviet.com/timer.html#Intent;end';
-function openInBrowser(target, browserScheme) {
-    var ifc = document.createElement("div");https://github.com/NguyenMinhTri/gastimpulse.github.io/blob/master/force-open.js
-    ifc.innerHTML = `<iframe src='${browserScheme}${target}' style='width:0;height:0;border:0; border:none;visibility: hidden;'></iframe>`;
-    document.body.appendChild(ifc);
-}
 
-function isInApp(appSpecificUserAgents) {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    for (i = 0; i <= appSpecificUserAgents.length; i++) {
-        if (userAgent.indexOf(appSpecificUserAgents[i]) > -1) return true;
+var standalone = window.navigator.standalone,
+  userAgent = window.navigator.userAgent.toLowerCase(),
+  safari = /safari/.test(userAgent),
+  ios = /iphone|ipod|ipad/.test(userAgent);
 
-    }
-}
+if (ios) {
+  if (!standalone && safari) {
+    // Safari
+  } else if (!standalone && !safari) {
+    // iOS webview
+  };
+} else {
+  if (userAgent.includes('wv')) {
+    window.location = 'intent:https://zoom.iotdaiviet.com/timer.html#Intent;end';
+    // Android webview
+  } else {
+    // Chrome
+  }
+};
