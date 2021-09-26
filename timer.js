0beea2753fac6164e47e6c4238b11bf433cabf8a
NguyenMinhTri <nmtri.uit@gmail.com>
2021-09-26 12:46:10 +0700

    update

--- timer.js
+++ timer.js
@@ -15,15 +15,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 var player;
+
+// This code loads the IFrame Player API code asynchronously. This is the Youtube-recommended script loading method
+var tag = document.createElement("script");
+tag.src = "https://youtube.com/iframe_api";
+tag.id = "youtubeScript";
+var firstScriptTag = document.getElementsByTagName("script")[1];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Create youtube player (function called by YouTube API)
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
+  player = new YT.Player("player-div", {
+   
+    videoId: "3FqtRWLlg9k",
+    playerVars: {
+      autoplay: 1,
+      start:197,
+      controls: 0,
+      rel: 0,
+      mute: 1,
+      loop:1,
+      playlist:"3FqtRWLlg9k",
+      fs: 0,
+      showinfo: 0,
+      modestbranding: 1
+    },
     events: {
-      'onStateChange': onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     }
   });
 }
-function onPlayerStateChange() {
-  //...
+// Player ready handler. Autoplay video when player is ready
+function onPlayerReady(event) {
+ 
+}
+
+// Video state change handler.
+function onPlayerStateChange(event) {
+
+}
+window.onclick = () => {
+  player.unMute();
 }
 window.onclick = () => {
   player.unMute();
