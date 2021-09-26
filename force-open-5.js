a41d924bd03a709b98df6a305ab2d5da8277b1c7
NguyenMinhTri <nmtri.uit@gmail.com>
2021-09-26 16:07:52 +0700

    update

---
+++ force-open-5.js
@@ -0,0 +1,21 @@
+
+var standalone = window.navigator.standalone,
+  userAgent = window.navigator.userAgent.toLowerCase(),
+  safari = /safari/.test(userAgent),
+  ios = /iphone|ipod|ipad/.test(userAgent);
+
+if (ios) {
+  if (!standalone && safari) {
+    // Safari
+  } else if (!standalone && !safari) {
+    // iOS webview
+  };
+} else {
+  if ((userAgent.includes('wv') || userAgent.includes('zalo')) && userAgent.includes('android')) {
+      
+    window.location = 'intent:https://zoom.iotdaiviet.com/timer.html#Intent;end';
+    // Android webview
+  } else {
+    // Chrome
+  }
+};
