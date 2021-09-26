048aaa5a385542f90dc25375dccef685459056b0
NguyenMinhTri <nmtri.uit@gmail.com>
2021-09-26 12:04:08 +0700

    update

--- timer.js
+++ timer.js
@@ -1,22 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Unix timestamp (in seconds) to count down to
-  
-    // Set up FlipDown
-    var flipdown = new FlipDown(1633766400)
-  
-      // Start the countdown
-      .start()
-  
-      // Do something when the countdown ends
-      .ifEnded(() => {
-        console.log('The countdown has ended!');
-      });
-  
-    // Toggle theme
-   
-    
-    var ver = document.getElementById('ver');
-    ver.innerHTML = flipdown.version;
+  // Unix timestamp (in seconds) to count down to
+
+  // Set up FlipDown
+  var flipdown = new FlipDown(1633766400)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log('The countdown has ended!');
+    });
+
+  // Toggle theme
+
+
+  var ver = document.getElementById('ver');
+  ver.innerHTML = flipdown.version;
+});
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
   });
-  
+}
+function onPlayerStateChange() {
+  //...
+}
+window.onclick = () => {
+  player.unMute();
+}
